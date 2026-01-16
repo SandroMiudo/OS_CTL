f76b260894b3ab7d90549dabcc82b52e44a557d3
SandroMiudo <sandromiudo0@gmail.com>
2026-01-16 02:29:53 +0100

    modifed interface, to additional contain flags and a custom callback

--- source/display/display_api.h
+++ source/display/display_api.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+#define WHITE 0 // off
+#define BLACK 1 // on
+
 typedef enum {
     DISPLAY_DRAW_IMAGE,
     DISPLAY_SET_PIXEL,
@@ -11,13 +14,17 @@ typedef enum {
     DISPLAY_QUERY_HEIGHT
 } display_command_t;
 
+typedef enum {
+    DISPLAY_WNO = 1// non blocking
+} display_flags_t;
+
 // For DISPLAY_DRAW_IMAGE
 typedef struct {
     int x;
     int y;
     int width;
     int height;
-    const uint32_t* buffer;
+    uint32_t* buffer;
 } cmd_draw_image_t;
 
 // For DISPLAY_SET_PIXEL
@@ -29,16 +36,32 @@ typedef struct {
 
 // For CLEAR_SCREEN
 typedef struct {
-    uint32_t color;
+    uint8_t on; // binary clear
 } cmd_clear_screen_t;
+
+// For FILL_SCREEN
+typedef struct {
+    uint32_t color;
+} cmd_fill_screen_t;
 
 // --------------------
 // Dedicated functions
 // --------------------
 
-void display_draw_image(const cmd_draw_image_t* cmd);
-void display_set_pixel(const cmd_set_pixel_t* cmd);
-void display_clear_screen(const cmd_clear_screen_t* cmd);
+typedef void (*display_callback_f)(char* msg);
+
+void display_draw_image(const cmd_draw_image_t* cmd, display_flags_t flags, 
+    display_callback_f cb, char* msg);
+void display_set_pixel(const cmd_set_pixel_t* cmd, display_flags_t flags, 
+    display_callback_f cb, char* msg);
+void display_clear_screen(const cmd_clear_screen_t* cmd, display_flags_t flags, 
+    display_callback_f cb, char* msg);
+void display_fill_screen(const cmd_fill_screen_t* cmd, display_flags_t flags, 
+    display_callback_f cb, char* msg);
+
+// --------------------
+// Query functions
+// --------------------
 
 int display_query_width(void);
 int display_query_height(void);
